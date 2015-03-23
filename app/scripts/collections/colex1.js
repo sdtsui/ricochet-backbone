b70936ca7d4f0b33cd47bc7760732a808b778ea9
sdtsui <danielt213@gmail.com>
2015-03-22 19:57:04 -0700

    added example collection

---
+++ app/scripts/collections/colex1.js
@@ -0,0 +1,14 @@
+/*global RicochetBackbone, Backbone*/
+
+RicochetBackbone.Collections = RicochetBackbone.Collections || {};
+
+(function () {
+    'use strict';
+
+    RicochetBackbone.Collections.Colex1 = Backbone.Collection.extend({
+
+        model: RicochetBackbone.Models.Colex1
+
+    });
+
+})();
---
+++ test/collections/colex1.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('Colex1 Collection', function () {
+
+    beforeEach(function () {
+        this.Colex1Collection = new RicochetBackbone.Collections.Colex1();
+    });
+
+});
